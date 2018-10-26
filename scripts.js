acf0800b0e7356fc2998dc3979d59d4de396469b
Patrick Hogan <phogan110@live.com>
2018-10-26 08:35:46 -0600

    created `refID` to give each h3 a unique id; started planning out 'click' event

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
-var h3Num = 1
+var h3Num = 1;
+var friends = ['Steph', 'Julius', 'Glenn', 'Vloshko'];
+var locationName = ['bed room', 'kitchen', 'bath room', 'living room', 'basement', 'attic', 'front yard', 'back yard', 'office', 'garage'];
+var weaponName = ['lamp', 'chair', 'bottle', 'amibo', 'box', 'multitool', 'screw driver', 'can', 'keyboard', 'vacuum', 'cleaining products', 'froot loops', 'rope', 'white vinegar', 'shelf', 'trash can', 'desktop', 'tv', 'monitor', 'labs'];
+var refId = 0;
 
 function pageLoad() {
     for (i = 0; i < 100; i++) {
         var h3 = document.createElement('h3');
-        var trackText = document.createTextNode(h3Num)
-        var body = document.getElementsByTagName('body');
-        body[0].appendChild(h3);
+        var trackText = document.createTextNode('Accusation ' + h3Num);
+        var body = document.body;
+        
+        body.appendChild(h3);
+        h3.id = refId;
+        refId++;
         h3.appendChild(trackText);
         h3Num++;
     }
+    // document.getElementsByTagName('h3').addEventListener('click', function(e) {
+    //     alert('I accuse ' + friends + ', with the ' + weaponName + ' in the ' + locationName + '!')
+    // });
 };
 
 pageLoad();
+
