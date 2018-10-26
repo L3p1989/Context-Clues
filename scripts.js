ca7cd6230418cb3ca5a44f633ddc2d59e14bdeb6
Patrick Hogan <phogan110@live.com>
2018-10-26 09:32:01 -0600

    finished 'click' event; added final comments. lab finished

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 var h3Num = 1;
-var friends = ['Steph', 'Julius', 'Glenn', 'Vloshko'];
+var friends = ['Steph', 'Julius', 'Glenn', 'Vloshko', 'Bladebuddy'];
 var locationName = ['bed room', 'kitchen', 'bath room', 'living room', 'basement', 'attic', 'front yard', 'back yard', 'office', 'garage'];
 var weaponName = ['lamp', 'chair', 'bottle', 'amibo', 'box', 'multitool', 'screw driver', 'can', 'keyboard', 'vacuum', 'cleaining products', 'froot loops', 'rope', 'white vinegar', 'shelf', 'trash can', 'desktop', 'tv', 'monitor', 'labs'];
-var refId = 0;
 
 function pageLoad() {
     for (i = 0; i < 100; i++) {
@@ -10,16 +9,18 @@ function pageLoad() {
         var trackText = document.createTextNode('Accusation ' + h3Num);
         var body = document.body;
         
-        body.appendChild(h3);
-        h3.id = refId;
-        refId++;
-        h3.appendChild(trackText);
+        body.appendChild(h3);//appends h3 to body
+        h3.appendChild(trackText);//appends trackText to h3
         h3Num++;
+
+        iEvent(i);
+
+        function iEvent(i) {
+            return document.getElementsByTagName('h3')[i].addEventListener('click', function() {
+                alert('I accuse ' + friends[i % friends.length] + ', with the ' + weaponName[i % weaponName.length] + ' in the ' + locationName[i % locationName.length] + '!')
+            })
+        };//adds click function with text
     }
-    // document.getElementsByTagName('h3').addEventListener('click', function(e) {
-    //     alert('I accuse ' + friends + ', with the ' + weaponName + ' in the ' + locationName + '!')
-    // });
-};
+};//on call this function adds 100 h3s with a click event showing the text behind the accusation
 
 pageLoad();
-
